a7177ea9e5a137edd12ef72b2eb9b0b7bf744a30
Andrei Korigodski <akorigod@gmail.com>
2019-01-18 15:28:00 +0300

    main: specify accurate dimensions of test sample

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 
 from polymech import compression
@@ -5,7 +6,9 @@ from polymech.sources import kechekyan
 
 
 dataset = kechekyan('test.xlsx')
-results = compression.analyze(dataset, 14, 40, 5)
+diameter_mm = 8.4
+cross_section_sq_mm = math.pi * diameter_mm ** 2 / 4
+results = compression.analyze(dataset, 13.47, cross_section_sq_mm, 5.0)
 print(results)
 plt.plot(*results.processed_dataset.T)
 plt.plot(*zip(results.young_modulus.first_point, results.young_modulus.second_point), 'r-')
