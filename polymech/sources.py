71d99ae7bb3a3b9073d2ea1a3bc512d0c2d822df
Andrei Korigodski <akorigod@gmail.com>
2019-01-20 02:42:50 +0300

    sources: add function to load samples metadata from yaml

--- polymech/sources.py
+++ polymech/sources.py
@@ -1,6 +1,7 @@
-from typing import BinaryIO
+from typing import BinaryIO, TextIO
 import numpy as np
 import xlrd
+from ruamel import yaml
 
 
 def kechekyan(xlsx_fd: BinaryIO) -> np.ndarray:
@@ -9,3 +10,20 @@ def kechekyan(xlsx_fd: BinaryIO) -> np.ndarray:
     timestamps = sheet.col_values(6, 1)
     forces = sheet.col_values(7, 1)
     return np.column_stack((timestamps, forces))
+
+
+def yaml_metadata(yaml_fd: TextIO):
+    yaml_obj = yaml.YAML(typ='safe')
+    parsed = list(yaml_obj.load_all(yaml_fd))
+    if len(parsed) == 1:
+        return parsed[0]
+    elif len(parsed) == 2:
+        common_properties = parsed[0]
+        samples = parsed[1]
+        for sample in samples:
+            for property_name, property_value in common_properties.items():
+                if property_name not in sample:
+                    sample[property_name] = property_value
+        return samples
+    else:
+        raise Exception(f'Expected one or two documents in yaml file, got {len(parsed)}')
