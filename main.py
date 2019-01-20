4b1a53d77cafd652f23dd63c31edf147b49eec1a
Andrei Korigodski <akorigod@gmail.com>
2019-01-21 00:45:35 +0300

    main: analyze batch from yaml file

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import math
-import matplotlib.pyplot as plt
+import json
+import csv
 
 from polymech import compression
-from polymech.sources import kechekyan
+from polymech.sources import kechekyan, yaml_metadata
+from polymech.plot import save_plot
 
+with open('samples.yaml') as f:
+    samples = yaml_metadata(f)
 
-with open('tests/data/01.xlsx', 'rb') as f:
-    dataset = kechekyan(f)
-diameter_mm = 8.4
-cross_section_sq_mm = math.pi * diameter_mm ** 2 / 4
-results = compression.analyze(dataset, 13.47, cross_section_sq_mm, 5.0)
-print(results)
-plt.plot(*results.processed_dataset.T)
-plt.plot(*zip(results.young_modulus.first_point, results.young_modulus.second_point), 'r-')
-plt.plot(results.yield_point.elongation, results.yield_point.tension, 'ro')
-plt.show()
+for sample in samples:
+    cross_section_sq_mm = math.pi * sample['diameter'] ** 2 / 4
+    with open(sample['file'], 'rb') as f:
+        results = compression.analyze(
+            kechekyan(f), sample['length'], cross_section_sq_mm, sample['compression_rate']
+        )
+    sample['young_modulus'] = results.young_modulus.modulus / 1e9
+    sample['yield_elongation'] = results.yield_point.elongation
+    sample['yield_strength'] = results.yield_point.tension / 1e6
+    sample['arzhakov'] = results.yield_point.elongation / (results.yield_point.tension / results.young_modulus.modulus)
+    with open(f"plots/{sample['name']}.png", 'wb') as f:
+        save_plot(results, f)
+with open('results.json', 'w') as f:
+    f.write(json.dumps(samples, indent=4, ensure_ascii=False))
+with open('results.csv', 'w') as f:
+    w = csv.DictWriter(f, sample.keys())
+    w.writeheader()
+    w.writerows(samples)
