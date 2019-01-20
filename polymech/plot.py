1b26f692444fb19388b302f5b02747b8d022209f
Andrei Korigodski <akorigod@gmail.com>
2019-01-21 01:16:33 +0300

    plot: make vars more verbose

--- polymech/plot.py
+++ polymech/plot.py
@@ -8,12 +8,12 @@ from polymech.compression import CompressionResults
 def stress_strain(results: CompressionResults) -> Figure:
     """
     To save the plot use Figure.savefig(file) method,
-    where file is a path string or a BinaryIO
+    where file is a path string or a BinaryIO.
     """
-    fig = Figure()
-    backend.FigureCanvasAgg(fig)
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(*results.processed_dataset.T)
-    ax.plot(*zip(results.young_modulus.first_point, results.young_modulus.second_point), 'r-')
-    ax.plot(results.yield_point.elongation, results.yield_point.tension, 'ro')
-    return fig
+    figure = Figure()
+    backend.FigureCanvasAgg(figure)
+    axes = figure.add_subplot(1, 1, 1)
+    axes.plot(*results.processed_dataset.T)
+    axes.plot(*zip(results.young_modulus.first_point, results.young_modulus.second_point), 'r-')
+    axes.plot(results.yield_point.elongation, results.yield_point.tension, 'ro')
+    return figure
